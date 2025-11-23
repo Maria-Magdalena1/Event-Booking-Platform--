585bb01a0971e1335452c10f576ae1c02642d46a
Maria-Magdalena1 <mariamagdalena.vladimirova@gmail.com>
2025-11-23 14:58:45 +0200

    feat(main-app): add test folder

---
+++ main-app/src/test/java/bg/softuni/eventbookingplatform/EventBookingPlatformApplicationTests.java
@@ -0,0 +1,14 @@
+package bg.softuni.eventbookingplatform;
+
+import main.EventBookingPlatformApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = EventBookingPlatformApplication.class)
+class EventBookingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
