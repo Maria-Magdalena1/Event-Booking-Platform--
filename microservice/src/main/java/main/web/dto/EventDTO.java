5a157e26f3d5c8ad4a7bb8a82443290199fb3a52
Maria-Magdalena1 <mariamagdalena.vladimirova@gmail.com>
2025-11-23 13:31:34 +0200

    feat(microservice): add web/dto classes

---
+++ microservice/src/main/java/main/web/dto/EventDTO.java
@@ -0,0 +1,18 @@
+package main.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventDTO {
+    private UUID id;
+    private String name;
+    private int totalSeats;
+    private double price;
+
+}
