c5de3195782267efb8cd2a3ba6fb8c8783c4f5c7
Maria-Magdalena1 <mariamagdalena.vladimirova@gmail.com>
2025-11-25 18:12:08 +0200

    feat(tests): add AnalyticsMicroserviceApplicationTests in main package

---
+++ microservice/src/test/java/main/AnalyticsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = AnalyticsMicroserviceApplication.class)
+class AnalyticsMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
