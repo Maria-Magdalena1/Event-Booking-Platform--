97c757724f7d6b7e233646f76e5dbbb2a05e6d56
Maria-Magdalena1 <mariamagdalena.vladimirova@gmail.com>
2025-12-03 14:30:59 +0200

    refactor(microservice): move AnalyticsController to web package

--- microservice/src/main/java/main/AnalyticsMicroserviceApplication.java
+++ microservice/src/main/java/main/AnalyticsMicroserviceApplication.java
@@ -2,10 +2,8 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-
 public class AnalyticsMicroserviceApplication {
 
     public static void main(String[] args) {
--- microservice/src/main/java/main/web/AnalyticsController.java
+++ microservice/src/main/java/main/web/AnalyticsController.java
@@ -1,4 +1,4 @@
-package main.controller;
+package main.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
